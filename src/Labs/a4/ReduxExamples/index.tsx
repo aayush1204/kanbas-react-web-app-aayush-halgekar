7fbd0c1d68d3f379ef4b2ceef6244f2896ea3986
Aayush Work <aayush@catalan.ai>
2024-03-21 18:39:05 -0400

    Added labs

--- src/Labs/a4/ReduxExamples/index.tsx
+++ src/Labs/a4/ReduxExamples/index.tsx
@@ -1,9 +1,24 @@
 import React from "react";
-
+import { useSelector, useDispatch } from "react-redux";
+import AddRedux from "./AddRedux";
+import CounterRedux from "./CounterRedux";
+import HelloRedux from "./HelloRedux";
+import TodoForm from "./todos/TodoForm";
+import TodoItem from "./todos/TodoItem";
+import TodoList from "./todos/TodoList";
+import { LabState } from "../../store";
 const ReduxExamples = () => {
+
+  const { todo } = useSelector((state: LabState) => state.todosReducer);
   return(
     <div>
       <h2>Redux Examples</h2>
+      <AddRedux/>
+      <CounterRedux/>
+      <HelloRedux/>
+      <TodoForm/>
+      <TodoItem todo ={ todo}/>
+      <TodoList/>
     </div>
   );
 };
--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -2,8 +2,17 @@
 import React from "react";
 import PassingFunctions from "./PassingFunctions";
 import ReduxExamples from "./ReduxExamples";
-
-
+import ArrayStateVariable from "./ArrayStateVariable";
+import BooleanStateVariables from "./BooleanStateVariables";
+import ChildStateComponent from "./ChildStateComponent";
+import ClickEvent from "./ClickEvent";
+import Counter from "./Counter";
+import DateStateVariable from "./DateStateVariable";
+import EventObject from "./EventObject";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import StringStateVariables from "./StringStateVariables";
 
 const Assignment4 = () => {
     function sayHello() {
@@ -14,6 +23,16 @@ const Assignment4 = () => {
     <>
       <h1>Assignment 4</h1>
       <PassingFunctions theFunction={sayHello} />
+      <ArrayStateVariable/>
+      <BooleanStateVariables/>
+      <ClickEvent/>
+      <Counter/>
+      <DateStateVariable/>
+      <EventObject/>
+      <ObjectStateVariable/>
+      <ParentStateComponent/>
+      <PassingDataOnEvent/>
+      <StringStateVariables/>
       <ReduxExamples/>
     </>
   );
